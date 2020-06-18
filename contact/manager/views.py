6622c26bafd37bbb312eaca61a265222cfad65e5
alcantara2575 <alcantara2575@gmail.com>
2020-06-18 17:18:23 -0400

    [dev #3] Example of use email

--- contact/manager/views.py
+++ contact/manager/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect 
+from django.core.mail import send_mail, get_connection
 from .forms import ContactForm
 from .models import ContactData
 
@@ -10,6 +11,16 @@ def contact(request):
 	if form.is_valid():
 		new_contact= form.save()
 		#create advise email with data
+		con = get_connection('django.core.mail.backends.console.EmailBackend')
+		send_mail(
+			'New user contact',
+			'Name: ' + new_contact.name + '\nEmail: '+ new_contact.email + 
+			'\nSubject: ' + new_contact.subject + '\nMessage: ' + new_contact.message + 
+			'\nLink to access: http://datwit.com/contact/data/' + str(new_contact.ref_number),
+			'noreply@gmail.com',
+			['siteowner@example.com'],
+			connection=con,
+			)
 		return redirect('data/' + str(new_contact.ref_number))
 	context= {'form': form }
 	return render(request, 'manager/contact.html', context)

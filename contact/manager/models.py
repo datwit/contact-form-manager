2520361e17c0f5a2b605c2fd898ae7af0877d158
alcantara2575 <alcantara2575@gmail.com>
2020-06-17 23:57:38 -0400

    [dev #1] Update models

--- contact/manager/models.py
+++ contact/manager/models.py
@@ -1,9 +1,17 @@
+import uuid
 from django.db import models
 
 # Create your models here.
 
 class ContactData(models.Model):
+	ref_number= models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email= models.EmailField()
     name= models.CharField(max_length=100)
     subject= models.CharField(max_length=200)
-    message= models.CharField(max_length=10000)
+    message= models.TextField()
+    attended= models.BooleanField(default= False)
+    created_at= models.DateTimeField(auto_now_add= True)
+    updated_at= models.DateTimeField(auto_add= True)
+    access_record= models.TextField()
+    
+    

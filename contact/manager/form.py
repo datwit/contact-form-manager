6f87feea96ff628f2b05a9ba0853a2fda0469357
alcantara2575 <alcantara2575@gmail.com>
2020-06-18 15:30:30 -0400

    [dev #2] Saving data and show

--- contact/manager/form.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import ContactData
-
-class ContactForm(forms.ModelForm):
-    class Meta:
-        model= ContactData
-        fields= ["email", "name", "subject", "message"]
--- contact/manager/views.py
+++ contact/manager/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect 
+from .forms import ContactForm
+from .models import ContactData
 
 # Create your views here.
+
+
+def contact(request):
+	form= ContactForm(request.POST or None)
+	if form.is_valid():
+		new_contact= form.save()
+		#create advise email with data
+		return redirect('data/' + str(new_contact.ref_number))
+	context= {'form': form }
+	return render(request, 'manager/contact.html', context)
+
+def data(request, contactID):
+	element=ContactData.objects.get(ref_number= contactID)
+	context={'element': element}
+	return render(request, 'manager/data.html', context)

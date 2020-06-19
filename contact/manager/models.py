3e3d57bfd99f08ba8a9a55dafa0a8a03e5b6d5f6
alcantara2575 <alcantara2575@gmail.com>
2020-06-19 08:33:10 -0400

    [dev #1] Fix name for reference field

--- contact/manager/models.py
+++ contact/manager/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 
 class ContactData(models.Model):
-	ref_number= models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+	ref_hash= models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 	email= models.EmailField(blank= False)
 	name= models.CharField(max_length=100)
 	subject= models.CharField(max_length=200, blank= False)

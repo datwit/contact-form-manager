6f5322da8c68a20cf4a94bf66b3d148d73b16ef8
Yoel Benítez Fonseca <ybenitezf@gmail.com>
2020-09-14 11:17:45 -0400

    test with real credentials

--- application/models.py
+++ application/models.py
@@ -1,16 +1,15 @@
 from pynamodb.models import Model
 from pynamodb.attributes import UnicodeAttribute
 from flask import current_app
-import os
 
 class Contact(Model):
     class Meta:
         table_name = 'Contact.{}'.format(current_app.config['STAGE'])
         billing_mode = 'PAY_PER_REQUEST'
         host = current_app.config['DYNDB']
-        aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
-        aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
-        region = os.getenv('AWS_REGION')
+        # aws_access_key_id = current_app.config['AWS_ACCESS_KEY_ID']
+        # aws_secret_access_key = current_app.config['AWS_SECRET_ACCESS_KEY']
+        # region = current_app.config['AWS_REGION']
 
     email = UnicodeAttribute(hash_key=True)
     name = UnicodeAttribute()

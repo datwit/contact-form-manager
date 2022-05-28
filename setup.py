93548f04efedaf89999f4fe84180ab4ac6e97aa9
Yoel Benitez Fonseca <ybenitezf@gmail.com>
2022-05-28 15:20:41 -0400

    refactor: basic python package
    Basic requirements for the python package
    
    related to datwit/landing3#144

---
+++ setup.py
@@ -0,0 +1,37 @@
+"""Datwit contact form backend package"""
+from setuptools import find_packages, setup
+import os
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='datwit_contact_form',
+    version='0.0.0',
+    url='https://github.com/datwit/contact-form-manager',
+    license='GPL',
+    author='Yoel Benítez Fonseca',
+    author_email='ybenitezf@gmail.com',
+    description='Datwit contact form backend',
+    long_description=read('README.md'),
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    setup_requires=['pytest-runner'],
+    tests_require=[
+        'pytest',
+        'pytest-cov'
+    ],
+    python_requires=">=3.8",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Other Audience",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)

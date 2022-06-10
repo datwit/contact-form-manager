b1a32f50abafd22efe663a3780adb40910a914d1
Yoel Benitez Fonseca <ybenitezf@gmail.com>
2022-06-10 15:03:35 -0400

    Bump version: 0.0.2 → 0.0.3

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name='datwit_contact_form',
-    version='0.0.2',
+    version='0.0.3',
     url='https://github.com/datwit/contact-form-manager',
     license='GPL',
     author='Yoel Benítez Fonseca',
